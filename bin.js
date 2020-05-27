aa667f03a21ec67b1c3cf64e4332ec943b7b093f
Roger Silva <roger.silva@uip.edu.pa>
2020-05-27 03:44:24 -0500

    pkg compatible

--- bin.js
+++ bin.js
@@ -6,8 +6,7 @@ var songnames = [];
 var songsjson = [];
 var decisiones = [];
 
-
-const settings = require('./settings'); //import settings
+const settings = require(process.cwd() + '/settings.js'); //import settings
 //Settings
 const mp = settings.mp;
 const mdir = settings.mdir;

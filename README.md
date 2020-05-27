2c20273debd304eda248bb10d441152798ac6926
Roger Silva <roger.silva@uip.edu.pa>
2020-05-27 04:12:47 -0500

    Update README.md and spelling correction

--- README.md
+++ README.md
@@ -3,7 +3,18 @@ Twitch Bot that adds to a playlist or player queue, files that are locally store
 
 Supported players: MPC-HC and PotPlayer.
 
-## Instructions
+## Installation & use
+
+### Downloading releases
+
+* Download the release for your OS here: https://github.com/rsilva48/LocalMediaTwitchBot/releases/latest.
+
+* Extract the .zip file and modify **settings.js**.
+
+* Then just run the executable file.
+
+
+### Using source code
 
 * First, install Node.js, you can get it here: https://nodejs.org/.
 
@@ -19,4 +30,13 @@ npm install
 To run the bot use the following command on the CLI:
 ```
 node index.js
+```
+
+### Commands
+
+* **!play** - To add a file to the queue use the command followed by the name of the file.
+
+Example:
+```
+!play some song
 ```
--- settingstemplate.js
+++ settingstemplate.js
@@ -12,7 +12,7 @@ const chatOutput = true; //Enables or disable the bot output on the chat.
 const debugOutput = false; //Enables or disable the bot debug output on console.
 const decisionDebug = false; //Enables or disable the bot debug output on console related to the decisions when bot returns multiple results.
 const searchDebug = false; //Enables or disable the bot debug output on console related to the file search results.
-const conmsg = [`Greetings, ${botUsername} is connected, created by @Harunoki__48.`, `To add a file to the queue use the comnand "!play" then the name of the file.`] //Set startup messages to be send when connected.
+const conmsg = [`Greetings, ${botUsername} is connected, created by @Harunoki__48.`, `To add a file to the queue use the command "!play" then the name of the file.`] //Set startup messages to be send when connected.
 const ignorebot = true;
 //End of Settings
 

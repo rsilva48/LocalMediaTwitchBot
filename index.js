7680b1e3f622f94083e29562abaa944ae22d2e7f
Roger Silva <roger.silva@uip.edu.pa>
2020-05-29 06:54:47 -0500

    Better format

--- index.js
+++ index.js
@@ -29,16 +29,16 @@ const epmsg = settings.set.epmsg;
 
 //File Extensions
 var filesext = []
-const vidext = ['.mp4', '.mkv', '.flv', '.webm','.avi','.wmv','.mgp','.mpeg'];
+const vidext = ['.mp4', '.mkv', '.flv', '.webm', '.avi', '.wmv', '.mgp', '.mpeg'];
 const audext = ['.mp3', '.mka', '.flac', '.wav', '.aac', '.ogg', '.mp2', '.ac3'];
-if (selfilesext === 'both'){
+if (selfilesext === 'both') {
   filesext = vidext.concat(audext);
 } else if (selfilesext === 'audio') {
   filesext = audext;
-} else if (selfilesext === 'video'){
+} else if (selfilesext === 'video') {
   filesext = vidext
 } else {
-  filesext =[]
+  filesext = []
 }
 
 
@@ -51,7 +51,7 @@ const options = {
     cluster: 'aws',
     reconnect: true,
   },
-  identity:{
+  identity: {
     username: botUsername,
     password: oauthpass
   },
@@ -61,29 +61,27 @@ const options = {
 //Create TMI Client
 const client = new tmi.client(options);
 
-function con(output){
-  if(conOutput){console.log(output)};
+function con(output) {
+  if (conOutput) { console.log(output) };
 }
-function debug(output){
-  if(debugOutput){console.log(output)}
+function debug(output) {
+  if (debugOutput) { console.log(output) }
 }
 
 //Scans each Media Directory
 con('Indexing files...')
 debug('Lista de archivos:');
-mdirs.forEach(function(mdir){
-  fs.readdir(mdir, function (err, archivos){
-    if (err)
-    {return console('Error al escanear el directorio' + err);}
+mdirs.forEach(function (mdir) {
+  fs.readdir(mdir, function (err, archivos) {
+    if (err) { return console('Error al escanear el directorio' + err); }
     debug('Lista de archivos:')
-    var i=0;
-    archivos.forEach(function (archivo){
+    var i = 0;
+    archivos.forEach(function (archivo) {
       var ext = path.extname(archivo);
       var basesong = path.basename(archivo, ext);
-      if (ext.startsWith('.'))
-      {
-        filesext.forEach(function(searchext){
-          if (searchext.toLowerCase() == ext.toLowerCase()){
+      if (ext.startsWith('.')) {
+        filesext.forEach(function (searchext) {
+          if (searchext.toLowerCase() == ext.toLowerCase()) {
             songfiles.push(archivo);
             getAudioDurationInSeconds(`${mdir}${basesong}${ext}`).then((duration) => {
               var songjson = {
@@ -91,40 +89,42 @@ mdirs.forEach(function(mdir){
                 name: basesong,
                 extension: ext,
                 dir: mdir,
-                fullpath: mdir+basesong+ext,
+                fullpath: mdir + basesong + ext,
                 duration: duration
               };
               songsjson.push(songjson);
               debug('json:');
               debug(songjson);
-              })
-            debug(`[${i+1}]. ${basesong}`);
+            })
+            debug(`[${i + 1}]. ${basesong}`);
             debug(`${mdir}${basesong}${ext}`)
             songnames.push(basesong);
-            i+=1;
+            i += 1;
           }
         })
       }
     }
-  )
-  if(searchDebug){console.log(songsjson);}
+    )
+    if (searchDebug) { console.log(songsjson); }
   });
 })
 con('Files indexed.')
 
 //Add selected file to the player queue
-function addtoqueue (filejson) {
+function addtoqueue(filejson) {
   queue.push(filejson);
   //Media Player Commands
   var pp = `"${mp}" "${filejson.fullpath}" /add`;
   var mpc = `"${mp}" "${filejson.fullpath}" /add /fullscreen`;
   var { exec } = require("child_process");
-  if (path.basename(mp) == 'mpc-hc64.exe' || path.basename(mp) == 'mpc-hc.exe')
-    {console.log(`* ${mpc}`);
-    exec(mpc);}
-  if (path.basename(mp) == 'PotPlayerMini64.exe' || path.basename(mp) == 'PotPlayerMini.exe')
-    {console.log(`* ${pp}`);
-    exec(pp);}
+  if (path.basename(mp) == 'mpc-hc64.exe' || path.basename(mp) == 'mpc-hc.exe') {
+    console.log(`* ${mpc}`);
+    exec(mpc);
+  }
+  if (path.basename(mp) == 'PotPlayerMini64.exe' || path.basename(mp) == 'PotPlayerMini.exe') {
+    console.log(`* ${pp}`);
+    exec(pp);
+  }
 };
 
 //Set functions for message and connection
@@ -134,51 +134,54 @@ client.on('connected', connected);
 client.connect();
 
 //Connected function
-function connected (address, port) {
-  conmsg.forEach(function(msg){
-    if (chatOutput){client.action(channelname, msg);}
+function connected(address, port) {
+  conmsg.forEach(function (msg) {
+    if (chatOutput) { client.action(channelname, msg); }
   })
 };
 
 
 //Message function
-function message (channel, tags, msg, self) {
+function message(channel, tags, msg, self) {
   //Ignore Bot own messages
-  if (ignorebot){if(self) {return;}
-  if (tags.username == botUsername.toLowerCase()) {
-    return;
-  };}
+  if (ignorebot) {
+    if (self) { return; }
+    if (tags.username == botUsername.toLowerCase()) {
+      return;
+    };
+  }
 
   //Gets the messages of the chat and turn it to lower case to be then processed.
   const comando = msg.trim().toLowerCase();
 
   //Command output function
-  function comandos(comando){
+  function comandos(comando) {
     console.log(`* Command: ${comando}`);
   };
 
   //Function to send a message in chat
-  function chat(message){
-    if (chatOutput){
-    client.say(channel, message);
-  }
+  function chat(message) {
+    if (chatOutput) {
+      client.say(channel, message);
+    }
   }
 
   function searchsong() {
-      var busqueda = comando.substr(6);
+    var busqueda = comando.substr(6);
     if (busqueda == '' || busqueda == ' ') {
       chat(`@${tags.username} ${epmsg}`);
       con(`* ${tags.username} sent play command empty.`);
       return;
     }
 
-    if (searchDebug){songnames.forEach(function(song){
-      con(song.toLowerCase())
-      con(song.toLowerCase().includes(busqueda))
-    })
-  }
+    if (searchDebug) {
+      songnames.forEach(function (song) {
+        con(song.toLowerCase())
+        con(song.toLowerCase().includes(busqueda))
+      })
+    }
 
-    var resultados = songfiles.filter(function (archivo) { return archivo.toLowerCase().includes(busqueda);})
+    var resultados = songfiles.filter(function (archivo) { return archivo.toLowerCase().includes(busqueda); })
     con('\n* Resultados:')
     con(resultados)
 
@@ -186,8 +189,8 @@ function message (channel, tags, msg, self) {
       chat(`@${tags.username} no se encontraron resultados para "${busqueda}".`);
       con(`* ${tags.username} busco "${busqueda}" sin resultados.`);
     } else if (resultados.length == 1) {
-      songsjson.forEach(function(songjson){
-        if (songjson.name+songjson.extension == resultados[0]){
+      songsjson.forEach(function (songjson) {
+        if (songjson.name + songjson.extension == resultados[0]) {
           addtoqueue(songjson);
           var ext = path.extname(resultados[0]);
           chat(`@${tags.username} ha añadido ${path.basename(resultados[0], ext)} a la cola.`);
@@ -195,108 +198,121 @@ function message (channel, tags, msg, self) {
         }
       })
 
-  } else if (resultados.length > 1) {
+    } else if (resultados.length > 1) {
       var opciones = '';
-      resultados.forEach(function(cancion, i){
+      resultados.forEach(function (cancion, i) {
         var ext = path.extname(cancion);
-        opciones += `\n[${i+1}]. ${path.basename(cancion, ext)},`;
+        opciones += `\n[${i + 1}]. ${path.basename(cancion, ext)},`;
       })
-    decisiones.push({
-      'username':`${tags.username}`,
-      'opciones':resultados,
-      'busqueda':busqueda,
-      'opcioneschat':opciones
-    });
-    if (decisionDebug){console.log('\n* Decisiones:');
-    con(decisiones)
-    con('\n* Decisiones length:');
-    con(decisiones.length);}
-    chat(`@${tags.username} hay varios resultados para  "${busqueda}": ${opciones}\n. ¿Cual desea reproducir? Para cancelar la busqueda envíe "0"`);
-    con(`* ${tags.username} busco "${busqueda}" con varios resultados.`);
+      decisiones.push({
+        'username': `${tags.username}`,
+        'opciones': resultados,
+        'busqueda': busqueda,
+        'opcioneschat': opciones
+      });
+      if (decisionDebug) {
+        console.log('\n* Decisiones:');
+        con(decisiones)
+        con('\n* Decisiones length:');
+        con(decisiones.length);
+      }
+      chat(`@${tags.username} hay varios resultados para  "${busqueda}": ${opciones}\n. ¿Cual desea reproducir? Para cancelar la busqueda envíe "0"`);
+      con(`* ${tags.username} busco "${busqueda}" con varios resultados.`);
     }
     else {
       debug('Error')
-      }
-    }}
+    }
+  }
 
   //Test Command
-  if(comando === `${prefix}${testcmd}`) {
+  if (comando === `${prefix}${testcmd}`) {
     comandos(comando);
     chat(`${testresponse} @${tags.username}`);
     con(`* ${tags.username} ${testcmnd} ${testresponse}`);
   };
 
   //Queue Command
-  if(comando === `${prefix}${queuecmd}`){
+  if (comando === `${prefix}${queuecmd}`) {
     debug(queue);
     var qchat = ''
-    queue.forEach(function(qsong, qc){
-      if (qc==0){
-        qchat+=(`${npmsg}: ${qsong.name}`)
+    queue.forEach(function (qsong, qc) {
+      if (qc == 0) {
+        qchat += (`${npmsg}: ${qsong.name}`)
       }
-      else if(qc==1) {
-        qchat+=(`, ${nqmsg}: [${qc}]. ${qsong.name}`)
+      else if (qc == 1) {
+        qchat += (`, ${nqmsg}: [${qc}]. ${qsong.name}`)
       }
       else {
-        qchat+=(`, [${qc}]. ${qsong.name}`)
+        qchat += (`, [${qc}]. ${qsong.name}`)
       }
     })
     chat(qchat);
   }
 
   //Decision selection
-  if (decisiones.length >= 1 && !comando.startsWith(`${prefix}${playcmd} `)){
-    if (decisionDebug) {console.log('\n* Decisiones length:');
-    con(decisiones.length);
-    con('Seleccion:')}
-    decisiones.forEach(function(decision, i){
-      if (decisionDebug) {con(decision.username);}
-      if (decision.username == tags.username){
-        if (comando == 0){
+  if (decisiones.length >= 1 && !comando.startsWith(`${prefix}${playcmd} `)) {
+    if (decisionDebug) {
+      console.log('\n* Decisiones length:');
+      con(decisiones.length);
+      con('Seleccion:')
+    }
+    decisiones.forEach(function (decision, i) {
+      if (decisionDebug) { con(decision.username); }
+      if (decision.username == tags.username) {
+        if (comando == 0) {
           chat(`@${tags.username} has cancelado tu busqueda.`);
           con(`* ${tags.username} canceló la busqueda.`);
-          if (decisionDebug){console.log('Busqueda');
-          con(decision.busqueda)}
+          if (decisionDebug) {
+            console.log('Busqueda');
+            con(decision.busqueda)
+          }
           //Erases the decision once fullfiled
-          decisiones = decisiones.filter(function(value, index, arr){
+          decisiones = decisiones.filter(function (value, index, arr) {
             return !(value.busqueda == decision.busqueda && value.username == decision.username);
           })
         }
-        decision.opciones.forEach(function(elecciones, j){
-        if (decisionDebug){console.log(j + ' ' + elecciones)}
-        if (comando == j+1){
-          songsjson.forEach(function(song){
-            if (song.name+song.extension == elecciones){
-              addtoqueue(song);
-            }
-          })
-          var ext = path.extname(elecciones);
-          chat(`@${tags.username} ha añadido ${path.basename(elecciones, ext)} a la cola.`);}
+        decision.opciones.forEach(function (elecciones, j) {
+          if (decisionDebug) { console.log(j + ' ' + elecciones) }
+          if (comando == j + 1) {
+            songsjson.forEach(function (song) {
+              if (song.name + song.extension == elecciones) {
+                addtoqueue(song);
+              }
+            })
+            var ext = path.extname(elecciones);
+            chat(`@${tags.username} ha añadido ${path.basename(elecciones, ext)} a la cola.`);
+          }
           con(`* ${tags.username} añadio ${path.basename(elecciones, ext)}" a la cola.`);
-          if (decisionDebug){console.log('Busqueda');
-          con(decision.busqueda)}
+          if (decisionDebug) {
+            console.log('Busqueda');
+            con(decision.busqueda)
+          }
           //Erases the decision once fullfiled
-          decisiones = decisiones.filter(function(value, index, arr){
+          decisiones = decisiones.filter(function (value, index, arr) {
             return !(value.busqueda == decision.busqueda && value.username == decision.username);
-          })})}})}
-        else if (comando.startsWith(`${prefix}${playcmd} `)) {
+          })
+        })
+      }
+    })
+  }
+  else if (comando.startsWith(`${prefix}${playcmd} `)) {
     comandos(comando);
     // Pending selection check
-    if (decisiones.length >= 1){
+    if (decisiones.length >= 1) {
       con('Pending selection check')
       var matches = 0;
-      decisiones.forEach(function(decision){
-        if (decision.username == tags.username){
+      decisiones.forEach(function (decision) {
+        if (decision.username == tags.username) {
           con(`* Pending selection for @${decision.username}:`)
           con(`* Pending selections :${decision.opciones}`)
           matches++;
-          if (chatOutput){
+          if (chatOutput) {
             chat(`@${tags.username} Tienes una cancion pendiente por elegir.`);
             chat(`@${tags.username} hay varios resultados para  "${decision.busqueda}": ${decision.opcioneschat}\n. ¿Cual desea reproducir? Para cancelar la busqueda envíe "0"`);
           }
         }
       })
-      if (matches == 0){
+      if (matches == 0) {
         searchsong();
       }
     } else {
@@ -304,4 +320,4 @@ function message (channel, tags, msg, self) {
     }
     return;
   }
-};
+}
